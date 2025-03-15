44181f0b03a8176cf48f9d4f88e693c0f2bf2310
Huỳnh Khánh Duy <duyxanh2002@gmail.com>
2025-03-15 21:45:53 +0700

    update

--- src/pages/Test/Test.tsx
+++ src/pages/Test/Test.tsx
@@ -1,19 +1,19 @@
-import React, { useContext } from 'react'
-import AuthContext from '../../contexts/AuthContext'
-import { Typography } from 'antd'
+// import React, { useContext } from 'react'
+// import AuthContext from '../../contexts/AuthContext'
+// import { Typography } from 'antd'
 
-const { Title } = Typography
+// const { Title } = Typography
 
-function Test() {
-  const { name } = useContext(AuthContext)
+// function Test() {
+//   const { name } = useContext(AuthContext)
 
-  return (
-    <div style={{ padding: '20px' }}>
-      <Title level={2}>
-        Tên được lưu trong context: {name}
-      </Title>
-    </div>
-  )
-}
+//   return (
+//     <div style={{ padding: '20px' }}>
+//       <Title level={2}>
+//         Tên được lưu trong context: {name}
+//       </Title>
+//     </div>
+//   )
+// }
 
-export default Test
+// export default Test
