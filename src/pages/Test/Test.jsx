f423b5a8203582b5c62b77beb02bb3f9ade980ac
Huỳnh Khánh Duy <duyxanh2002@gmail.com>
2025-04-20 08:39:50 +0700

    test mocdata api n8n

--- src/pages/Test/Test.jsx
+++ src/pages/Test/Test.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useState, useEffect } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import Loader from '../../components/loader/Loader'
@@ -8,10 +8,13 @@ import './Test.scss'
 import { useNavigate } from 'react-router-dom'
 import Sky from '../../models3d/Sky'
 import Header from '../../components/Header/Header'
-import { Button } from 'antd'
+import { Button, message, Table } from 'antd'
+import { testMockData } from '../Home/services'
 
 const Test = () => {
     const navigate = useNavigate()
+    const [dataSource, setDataSource] = useState([])
+    const [loading, setLoading] = useState(false)
     const adjustSeeHouseForScreenSize = () => {
         let screenScale = null
         let screenPosition = [0, -6.5, -43]
@@ -24,6 +27,55 @@ const Test = () => {
         }
         return { screenScale, screenPosition, rotation }
     }
+    const columns = [
+        {
+            title: 'Id',
+            dataIndex: 'id', 
+            key: 'id'
+        },
+        {
+            title: 'Tên mèo',
+            dataIndex: 'ten_meo',
+            key: 'ten_meo'
+        },
+        {
+            title: 'Màu lông',
+            dataIndex: 'mau_long',
+            key: 'mau_long'
+        },
+        {
+            title: 'Tính cách',
+            dataIndex: 'tinh_cach',
+            key: 'tinh_cach'
+        },
+        {
+            title: 'Cân nặng',
+            dataIndex: 'can_nang_kg',
+            key: 'can_nang_kg'
+        },
+        {
+            title: 'Tuổi',
+            dataIndex: 'tuoi_nam',
+            key: 'tuoi_nam'
+        }
+    ]
+
+    const testMockDataN8N = async () => {
+        setDataSource([])
+        setLoading(true)
+        try {
+            const res = await testMockData()
+            console.log('mock_data_ne:', res)
+            message.success('Get mock data success')
+            setDataSource(res?.data?.cats)
+        } catch (error) {
+            message.error('Error when get mock data')
+            console.log('error:', error)
+        } finally {
+            setLoading(false)
+        }
+
+    }
 
     const { screenScale, screenPosition, rotation } = adjustSeeHouseForScreenSize()
 
@@ -31,7 +83,22 @@ const Test = () => {
         <>
             <Header />
             <div className="test-container">
+                <Button style={{ cursor: 'pointer', margin: '20px', color: 'green' }} onClick={() => testMockDataN8N()}>Test mock data</Button>
                 <Button style={{ cursor: 'pointer', margin: '20px', color: 'green' }} loading={true} onClick={() => navigate('/')}>Back home</Button>
+                <Table 
+                loading={loading}
+                 dataSource={dataSource}
+                  columns={columns}
+                  pagination={
+                    {
+                        pageSize: 10,
+                        total: dataSource.length,
+                        showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`,
+                        // showSizeChanger: true,
+                        // showQuickJumper: true,
+                    }
+                  }
+                  />
                 <Canvas
                     className='w-full h-full'
                     camera={{ near: 0.1, far: 100 }}
