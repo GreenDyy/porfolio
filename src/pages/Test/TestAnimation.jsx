43fa3c570ed37217ad6d878efdaa3e5d78c8d05a
KhanhDuy <duyxanh2002@gmail.com>
2025-06-30 13:51:02 +0700

    clone parallax Firewatch

--- src/pages/Test/TestAnimation.jsx
+++ src/pages/Test/TestAnimation.jsx
@@ -2,6 +2,22 @@ import React, { useState, useRef } from 'react'
 import './Test.scss'
 import { ButtonCustom } from '../../components'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import { Flex } from 'antd'
+import text_greend from '../../assets/images/parallax/text_greend.png'
+
+// factor={1} // nó sẽ chiếm 100vh
+const imagesForParalax = {
+    img1: 'https://www.firewatchgame.com/images/parallax/parallax0@2x.png',
+    img2: 'https://www.firewatchgame.com/images/parallax/parallax1@2x.png',
+    // img3: 'https://www.firewatchgame.com/images/parallax/parallax2@2x.png', //chữ FIREWATCH
+    img3: text_greend,
+    img4: 'https://www.firewatchgame.com/images/parallax/parallax3@2x.png',
+    img5: 'https://www.firewatchgame.com/images/parallax/parallax4@2x.png',
+    img6: 'https://www.firewatchgame.com/images/parallax/parallax5@2x.png',
+    img7: 'https://www.firewatchgame.com/images/parallax/parallax6@2x.png',
+    img8: 'https://www.firewatchgame.com/images/parallax/parallax7@2x.png',
+    img9: 'https://www.firewatchgame.com/images/parallax/parallax8@2x.png'
+}
 
 const TestAnimation = () => {
     const refTest = useRef(null)
@@ -9,37 +25,57 @@ const TestAnimation = () => {
     return (
         <div className='test-container'>
             {/* <ButtonCustom /> */}
-            <Parallax pages={4} ref={refTest}>
-                {/* <ParallaxLayer speed={3} style={{ backgroundColor: 'red' }}>
-                    <div style={{ width: '100%', height: '100%' }}>
-                        <img src="https://i.pinimg.com/736x/28/dc/36/28dc36d443030e5222e4b39118f18d4e.jpg" alt="" />
+            <Parallax pages={2} style={{ top: '0', left: '0' }} className="animation">
+                <ParallaxLayer offset={0} speed={0.25}>
+                    <div className="image" id="artback">
+                        <img className='img-paralax' src={imagesForParalax?.img1} alt="Parallax layer 1" />
                     </div>
                 </ParallaxLayer>
-                <ParallaxLayer offset={1.0} style={{ backgroundColor: 'blue' }} speed={3}>
-                    <div style={{ width: '100%', height: '100%' }}>
-                        <img src="https://i.pinimg.com/736x/8e/0c/28/8e0c28b320929ff9c23723e70785ec97.jpg" alt="" />
+                <ParallaxLayer offset={0} speed={0.3}>
+                    <div className="image" id="artback">
+                        <img className='img-paralax' src={imagesForParalax?.img2} alt="Parallax layer 2" />
                     </div>
-                </ParallaxLayer> */}
-
-                <ParallaxLayer
-                    speed={2} //càng lớn sẽ delay càng nhiều
-                    factor={1} // nó sẽ chiếm 100vh
-                    style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
-                    onClick={() => {
-                        refTest.current.scrollTo(1)
-                    }}
-                >
-                    <h1 style={{ fontFamily: 'sans-serif', fontSize: '100px' }}>My name is GreenD</h1>
                 </ParallaxLayer>
-
-                <ParallaxLayer speed={2} offset={1} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'blue' }}>
-                    <h1>I'm very happy</h1>
+                <ParallaxLayer offset={0} speed={-0.1}>
+                    <div className="image" id="artback">
+                        <img className='img-paralax' src={imagesForParalax?.img3} alt="Parallax layer 3" />
+                    </div>
                 </ParallaxLayer>
-                <ParallaxLayer
-                    sticky={{ start: 0.11, end: 0.2 }}
-                    offset={0} speed={12}
-                >
-                    <img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ2ljazJ3Zm5ibGo5ZzJiNmE1a3RzeThibjVoNTR5cDJzNmwxbXozeiZlcD12MV9naWZzX3RyZW5kaW5nJmN0PWc/sWJRqC2Wzn9mQyi0za/200.webp" alt="" />
+                <ParallaxLayer offset={0} speed={0.3}>
+                    <div className="image" id="artback">
+                        <img className='img-paralax' src={imagesForParalax?.img4} alt="Parallax layer 4" />
+                    </div>
+                </ParallaxLayer>
+                <ParallaxLayer offset={0} speed={0.35}>
+                    <div className="image" id="artback">
+                        <img className='img-paralax' src={imagesForParalax?.img5} alt="Parallax layer 5" />
+                    </div>
+                </ParallaxLayer>
+                <ParallaxLayer offset={0} speed={0.5}>
+                    <div className="image" id="artback">
+                        <img className='img-paralax' src={imagesForParalax?.img6} alt="Parallax layer 6" />
+                    </div>
+                </ParallaxLayer>
+                <ParallaxLayer offset={0} speed={0.45}>
+                    <div className="image" id="artback">
+                        <img className='img-paralax' src={imagesForParalax?.img7} alt="Parallax layer 7" />
+                    </div>
+                </ParallaxLayer>
+                <ParallaxLayer offset={0} speed={0.40}>
+                    <div className="image" id="artback">
+                        <img className='img-paralax' src={imagesForParalax?.img8} alt="Parallax layer 8" />
+                    </div>
+                </ParallaxLayer>
+                <ParallaxLayer offset={0} speed={0.35}>
+                    <div className="image" id="artback">
+                        <img className='img-paralax' src={imagesForParalax?.img9} alt="Parallax layer 9" />
+                    </div>
+                </ParallaxLayer>
+                <ParallaxLayer factor={1.5} offset={0.8} speed={0.25}>
+                    <Flex className='block-text'>
+                        <h1>GreenD</h1>
+                        <p>I'm a developer who likes to do different and cool things, learn from great people 😉. Oh, and I really like the color green!🍀</p>
+                    </Flex>
                 </ParallaxLayer>
             </Parallax>
         </div>
